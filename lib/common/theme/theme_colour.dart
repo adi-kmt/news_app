5f6054f226ff0730e30fa6c039719f45bc5237b2
Adithya Kamath <kamathis4@gmail.com>
2023-01-27 12:02:56 +0530

    Themes, text, colours and screen sizes fixed

---
+++ lib/common/theme/theme_colour.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class ThemeColour {
+  ThemeColour._();
+
+  static const Color primaryColour = Color.fromARGB(255, 245, 245, 225);
+  static const Color secondaryColour = Color.fromARGB(255, 247, 169, 0);
+  static const Color selectedColour = Color.fromARGB(255, 1, 2, 25);
+}
---
+++ lib/common/theme/theme_sizes.dart
@@ -0,0 +1,9 @@
+class ThemeSizes {
+  ThemeSizes._();
+
+  // Theme size based on Material Design
+  static const double dimen_14 = 14;
+  static const double dimen_16 = 16;
+  static const double dimen_20 = 20;
+  static const double dimen_24 = 24;
+}

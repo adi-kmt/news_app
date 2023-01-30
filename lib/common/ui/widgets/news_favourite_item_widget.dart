a022b6f850f9967d1ac7a3a2ecdf226e56dac75f
Adithya Kamath <kamathis4@gmail.com>
2023-01-30 12:28:38 +0530

    Widget items for source and favourite item

---
+++ lib/common/ui/widgets/news_favourite_item_widget.dart
@@ -0,0 +1,63 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:news_app/common/theme/theme_sizes.dart';
+import 'package:news_app/news_list_feature/domain/model/news_entity.dart';
+import 'package:news_app/utils/size_utils.dart';
+
+class NewsFavouriteItemWidget extends StatelessWidget {
+  final NewsArticleEntity newsArticleEntity;
+
+  const NewsFavouriteItemWidget({Key? key, required this.newsArticleEntity})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Container(
+          decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10.0),
+                  topRight: Radius.circular(40.0),
+                  bottomRight: Radius.circular(10.0),
+                  bottomLeft: Radius.circular(40.0))),
+          padding: const EdgeInsets.all(10),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 10),
+                child: CachedNetworkImage(
+                  imageUrl: newsArticleEntity.image,
+                  width: ThemeSizes.dimen_20.w,
+                  height: ThemeSizes.dimen_20.h,
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      newsArticleEntity.title,
+                      style: Theme.of(context).textTheme.subtitle1,
+                    ),
+                    Text(
+                      newsArticleEntity.description,
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                      style: Theme.of(context).textTheme.bodyText2,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/news_source_feature/ui/screen/source_list_screen.dart
+++ lib/news_source_feature/ui/screen/source_list_screen.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/common/ui/widgets/source_item_widget.dart';
+import 'package:news_app/news_source_feature/domain/model/news_source_entity.dart';
+
+final sources = [
+  NewsSourceEntity(name: "name", language: "language", country: "country"),
+  NewsSourceEntity(name: "name1", language: "language1", country: "country1"),
+  NewsSourceEntity(name: "name2", language: "language2", country: "country2"),
+];
 
 class SourceListScreen extends StatelessWidget {
   const SourceListScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Source List screen")));
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: ListView.builder(
+        itemBuilder: (context, index) =>
+            SourceItemWidget(newsSourceEntity: sources[index]),
+        itemCount: sources.length,
+        scrollDirection: Axis.vertical,
+      ),
+    );
   }
 }

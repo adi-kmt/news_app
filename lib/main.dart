66b995e085bdaf69e92b70bc32658d22c9f88883
Adithya Kamath <kamathis4@gmail.com>
2023-02-07 10:21:51 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -11,18 +11,3 @@ void main() async {
   await get_it.getItInstance<HiveInit>().init();
   runApp(const NewsApp());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Placeholder(),
-    );
-  }
-}
